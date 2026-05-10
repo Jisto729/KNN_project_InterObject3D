66fb88e477c90418fddd7e7264da868a62b44e41
Artem Vereninov <averenin@averenin-thinkpadx1carbongen12.tpbc.csb>
2026-05-10 13:23:26 +0200

    remove comments and scannet

--- InterObject3D/Minkowski/training/evaluation/compute_noc.py
+++ InterObject3D/Minkowski/training/evaluation/compute_noc.py
@@ -1,34 +1,42 @@
-
 import numpy as np
 
 
-def load_results(results_file, label=None, MAX_IOU=90, dataset=None, dataset_classes=None,
-                 exclude_classes=['wall', 'ceiling', 'floor', 'unlabelled', 'unlabeled'], dataset_name="scannet"):
+def load_results(
+    results_file,
+    label=None,
+    MAX_IOU=90,
+    dataset=None,
+    dataset_classes=None,
+    exclude_classes=["wall", "ceiling", "floor", "unlabelled", "unlabeled"],
+    dataset_name="semKITTI",
+):
     objects = {}
-    
+
     def process_objects(data):
         nonlocal objects
         for entry in data:
-            objects[entry[0].replace('scene', '') + '_' + entry[1]] = 1
+            objects[entry[0].replace("scene", "") + "_" + entry[1]] = 1
 
     def filter_objects_by_classes(dataset_, dataset_classes, exclude_classes):
-        
+
         mask = np.isin(dataset_classes, exclude_classes, invert=True)
-        print('total number of objects: ', np.shape(dataset_classes))
-        print('number of objects kept: ', sum(mask))
+        print("total number of objects: ", np.shape(dataset_classes))
+        print("number of objects kept: ", sum(mask))
         return dataset_[mask], dataset_classes[mask]
 
     if exclude_classes:
-        dataset, dataset_classes = filter_objects_by_classes(dataset, dataset_classes, exclude_classes)
+        dataset, dataset_classes = filter_objects_by_classes(
+            dataset, dataset_classes, exclude_classes
+        )
         process_objects(dataset)
     if label is not None:
         dataset = dataset[dataset_classes == label]
         objects = {}
         process_objects(dataset)
-        print('number of objects kept from class ', label, ": ", len(objects))
+        print("number of objects kept from class ", label, ": ", len(objects))
     else:
         process_objects(dataset)
-        print('number of objects kept: ', len(objects))
+        print("number of objects kept: ", len(objects))
 
     results_dict_KatIOU = {}
     num_objects = 0
@@ -39,15 +47,15 @@ def load_results(results_file, label=None, MAX_IOU=90, dataset=None, dataset_cla
     all_data = {}
     corrupted = 0
 
-    with open(results_file, 'r') as f:
+    with open(results_file, "r") as f:
         for line in f:
-            splits = line.rstrip().split(' ')
-            scene_name = splits[1].replace('scene', '')
+            splits = line.rstrip().split(" ")
+            scene_name = splits[1].replace("scene", "")
             object_id = splits[2]
             num_clicks = splits[3]
             iou = splits[4]
 
-            obj_key = scene_name + '_' + object_id
+            obj_key = scene_name + "_" + object_id
 
             if obj_key in objects:
                 all_object.setdefault(obj_key, 1)
@@ -59,17 +67,14 @@ def load_results(results_file, label=None, MAX_IOU=90, dataset=None, dataset_cla
                         num_objects += 1
                         ordered_clicks.append(float(num_clicks))
                 elif int(num_clicks) >= 20 and float(iou) >= 0:
-                
-                    if  float(iou) == 0 and dataset_name=="semKITTI":
-                        
-                        corrupted+=1 # no enough object instance points
-                    else:        
-
+                    if float(iou) == 0 and dataset_name == "semKITTI":
+                        corrupted += 1  # no enough object instance points
+                    else:
                         if obj_key not in results_dict_KatIOU:
                             results_dict_KatIOU[obj_key] = float(num_clicks)
                             num_objects += 1
                             ordered_clicks.append(float(num_clicks))
-                
+
                 results_dict_per_click.setdefault(num_clicks, 0)
                 results_dict_per_click_iou.setdefault(num_clicks, 0)
 
@@ -77,11 +82,18 @@ def load_results(results_file, label=None, MAX_IOU=90, dataset=None, dataset_cla
                 results_dict_per_click_iou[num_clicks] += float(iou)
 
     if not results_dict_KatIOU:
-        print('no objects to evaluate')
+        print("no objects to evaluate")
         return 0
 
     click_at_80 = sum(results_dict_KatIOU.values()) / len(results_dict_KatIOU.values())
-    print('click@', MAX_IOU, click_at_80, num_objects, len(all_object), len(results_dict_KatIOU))
+    print(
+        "click@",
+        MAX_IOU,
+        click_at_80,
+        num_objects,
+        len(all_object),
+        len(results_dict_KatIOU),
+    )
     clicks = [str(i) for i in range(21)]
     iou = [results_dict_per_click_iou[i] / results_dict_per_click[i] for i in clicks]
 
@@ -92,111 +104,28 @@ def load_results(results_file, label=None, MAX_IOU=90, dataset=None, dataset_cla
     return ordered_clicks, clicks, iou
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    datasets = ["semKITTI"]
 
-
-    datasets = ['scannet'] # 'scannet' 's3dis','semKITTI'
-
-
-    if 'scannet' in datasets:
-
-        label_scannet_benchmark = ['wall', 'floor', 'cabinet',
-                                   'bed',
-                                   'chair',
-                                   'sofa',
-                                   'table',
-                                   'door',
-                                   'window',
-                                   'bookshelf',
-                                   'picture',
-                                   'counter',
-                                   'desk',
-                                   'curtain',
-                                   'refridgerator',
-                                   'shower',
-                                   'curtain',
-                                   'toilet',
-                                   'sink',
-                                   'bathtub',
-                                   'otherfurniture']
-        label_scannet_all = {
-            'unlabelled', 'wall', 'floor', 'cabinet', 'bed', 'chair', 'sofa', 'table', 'door', 'window',
-            'bookshelf', 'picture', 'counter', 'blinds', 'desk', 'shelves', 'curtain', 'dresser', 'pillow',
-            'mirror', 'floormat', 'clothes', 'ceiling', 'books', 'refrigerator', 'television', 'paper',
-            'towel', 'showercurtain', 'box', 'whiteboard', 'person', 'nightstand', 'toilet', 'sink', 'lamp',
-            'bathtub', 'bag', 'otherstructure', 'otherfurniture', 'otherprop'
-        }
-
-        path = "./results/scannet_val/"
-        base_005_005 = 'baseline_all2_005_005_0_14.csv'
-        dataset_scannet = np.load(
-            './results/scannet_val/dataset_scannet_val.npy')
-        dataset_classes_scannet = np.loadtxt(
-            './results/scannet_val/dataset_scannet_val_classes2.txt',
-            dtype=str)
-
-        print('scannet')
-
-    
-        for iou_max in [80, 85, 90]:
-            print('\t All classes')
-
-            _, clicks_scannet, iou_scannet = load_results(path + base_005_005, None, iou_max, dataset_scannet,
-                                                          dataset_classes_scannet, None)
-            print('\t Seen classes')
-
-            load_results(path + base_005_005, None, iou_max, dataset_scannet, dataset_classes_scannet, exclude_classes=list(set(label_scannet_all)-set(label_scannet_benchmark)))
-
-            print('\t UNSeen classes')
-
-            load_results(path + base_005_005, None, iou_max, dataset_scannet, dataset_classes_scannet, exclude_classes=list(['unlabelled']+label_scannet_benchmark))
-
-
-            # print('Class Bed')
-            # _, clicks_scannet, iou_scannet = load_results(path + base_005_005, 'bed', iou_max, dataset_scannet,
-        #                                                dataset_classes_scannet, None)
-
-
-    elif "s3dis" in datasets:
-        label_s3dis = [ None, 'ceiling',
-                  'floor',
-                  'wall',
-                  'beam',
-                  'column',
-                  'window',
-                  'door',
-                  'table',
-                  'chair',
-                  'sofa',
-                  'bookcase',
-                  'board',
-                  'clutter']
-
-
-
-        base_005_005 = 's3dis_baseline_all2_005_0_14.csv'
-        path = "./results/s3dis_area5/"
-
-        dataset_s3dis = np.load('./results/s3dis_area5/dataset_area5.npy')
-        dataset_classes_s3dis = np.loadtxt(
-            './results/s3dis_area5/dataset_area5_classes.txt', dtype=str)
-
-        print('s3dis')
-
-        for iou_max in [80, 85, 90]:
-
-            load_results(path + base_005_005, None, iou_max, dataset_s3dis, dataset_classes_s3dis,None)
-
-    elif "semKITTI" in datasets:
+    if "semKITTI" in datasets:
         path = path = "./results/semanticKITTI/"
 
-        base_005_005 = 'semKITTI_baseline_all2_005_005_all_14.csv'
-        dataset_semKITTI = np.load('./results/semanticKITTI/dataset_01.npy')
+        base_005_005 = "semKITTI_baseline_all2_005_005_all_14.csv"
+        dataset_semKITTI = np.load("./results/semanticKITTI/dataset_01.npy")
         dataset_classes_semKITTI = np.loadtxt(
-            './results/semanticKITTI/dataset_01_classes.txt', dtype=str)
+            "./results/semanticKITTI/dataset_01_classes.txt", dtype=str
+        )
         print("SemanticKITTI")
         for iou_max in [80, 85, 90]:
-            print('++++')
-            print('base')
-            print('005 to 005 - excluded unlabeled')
-            load_results(path + base_005_005, None, iou_max, dataset_semKITTI, dataset_classes_semKITTI,None,"semKITTI")
+            print("++++")
+            print("base")
+            print("005 to 005 - excluded unlabeled")
+            load_results(
+                path + base_005_005,
+                None,
+                iou_max,
+                dataset_semKITTI,
+                dataset_classes_semKITTI,
+                None,
+                "semKITTI",
+            )
--- InterObject3D/Minkowski/training/run_stat.py
+++ InterObject3D/Minkowski/training/run_stat.py
@@ -8,76 +8,10 @@ from sklearn.metrics import average_precision_score
 from data_preparation.kitti import preprocess_data
 
 
-def generate_scenes_scannet():
-    PATH_TO_SCANNET_DATASET = 'data_preparation/dataset/scannet'
-    OUTPUT_DIR = 'data_preparation/processed_datasets_scannet/'
-
-    for folder_name in os.listdir(PATH_TO_SCANNET_DATASET):
-        print(f"Executing script for: {folder_name}")
-
-        command = [
-                "python", "data_preparation/main_scannet.py",
-                f"--name={folder_name}",
-                f"--path={PATH_TO_SCANNET_DATASET}",
-                f"--output_dir={OUTPUT_DIR}"
-            ]
-        
-        subprocess.run(command, check=True)
-
-    print("All directories completed.")
-
 def generate_scenes_kitti():
-    PATH_TO_KITTI_DATASET = 'data_preparation/dataset/kitti'
+    PATH_TO_KITTI_DATASET = "data_preparation/dataset/kitti"
     preprocess_data(PATH_TO_KITTI_DATASET)
 
-
-def run_inter_scannet():
-    
-    path_to_scenes = "data_preparation/processed_datasets_scannet/scenes_&_classes/"
-    masks = "data_preparation/processed_datasets_scannet/masks5x5/"
-    crops = "data_preparation/processed_datasets_scannet/crops5x5/"
-    pretraining_weights = config.pretraining_weights
-    used_model = config.used_model
-    dataset = config.dataset
-
-    for folder_name in os.listdir(path_to_scenes):
-        folder_path = os.path.join(path_to_scenes, folder_name)
-
-        if os.path.isdir(folder_path):
-
-            results_file_name = folder_name
-            
-            txt_files = glob.glob(os.path.join(folder_path, '*.txt'))
-            npy_files = glob.glob(os.path.join(folder_path, '*.npy'))
-
-            if not txt_files or not npy_files:
-                print(f"Skipping directory {folder_name}: Missing .txt or .npy file.")
-                continue
-
-            classes = txt_files[0]
-            scenes = npy_files[0]
-
-        print(f"Executing script for: {results_file_name}")
-
-        command = [
-                "python", "run_inter3d.py",
-                f"--dataset_classes={classes}",
-                f"--dataset_scenes={scenes}",
-                f"--dataset_folder_scene={crops}",
-                f"--dataset_folder_masks={masks}",
-                "--cubeedge=0.05",
-                f"--used_model={used_model}",
-                f"--pretraining_weights={pretraining_weights}",
-                f"--dataset={dataset}",
-                "--save_results_file=True",
-                f"--results_path=./dataset_result/{dataset}/",
-                f"--results_file_name={results_file_name}"
-            ]
-
-        # check - stop if run_inter3d.py ends with error
-        subprocess.run(command, check=True)
-
-    print("All directories completed.")
 
 def run_inter_semkitti():
     seq_num = config.seq_num
@@ -96,49 +30,52 @@ def run_inter_semkitti():
         print(f"Executing script for: {results_file_name}")
 
         command = [
-                "python", "run_inter3d.py",
-                f"--dataset_scenes={folder_path}",  # Pass the whole frame folder
-                "--cubeedge=0.05",
-                f"--used_model={used_model}",
-                f"--pretraining_weights={pretraining_weights}",
-                f"--dataset={dataset}",
-                "--save_results_file=True",
-                f"--results_path=./dataset_result/{dataset}/",
-                f"--results_file_name={results_file_name}"
-            ]
+            "python",
+            "run_inter3d.py",
+            f"--dataset_scenes={folder_path}",  # Pass the whole frame folder
+            "--cubeedge=0.05",
+            f"--used_model={used_model}",
+            f"--pretraining_weights={pretraining_weights}",
+            f"--dataset={dataset}",
+            "--save_results_file=True",
+            f"--results_path=./dataset_result/{dataset}/",
+            f"--results_file_name={results_file_name}",
+        ]
 
         # check - stop if run_inter3d.py ends with error
         subprocess.run(command, check=True)
 
     print("All directories completed.")
 
+
 def run_stat():
 
     result_file_paths = glob.glob(f"dataset_result/{config.dataset}/*.csv")
-    df_list = [pd.read_csv(f, sep='\s+', header=None, 
-                        names=['index', 'scene', 'obj_id', 'click', 'iou', 'confidence']) 
-            for f in result_file_paths]
+    df_list = [
+        pd.read_csv(
+            f,
+            sep="\s+",
+            header=None,
+            names=["index", "scene", "obj_id", "click", "iou", "confidence"],
+        )
+        for f in result_file_paths
+    ]
     df = pd.concat(df_list, ignore_index=True)
 
-
     target_clicks = [1, 2, 3, 5, 10, 20]
-  
 
     results = []
 
-
-
     for click in target_clicks:
-        click_data = df[df['click'] == click]
+        click_data = df[df["click"] == click]
 
         click_data = click_data[click_data != None]
 
         if len(click_data) == 0:
             continue
-        
-        iou_scores = click_data['iou']
-        conf_scores = click_data['confidence']
 
+        iou_scores = click_data["iou"]
+        conf_scores = click_data["confidence"]
 
         miou = iou_scores.mean()
 
@@ -146,17 +83,24 @@ def run_stat():
 
         sr50 = (iou_scores > 50.0).mean() * 100
 
-        thresholds = np.arange(50,100,5)
+        thresholds = np.arange(50, 100, 5)
         msr = np.mean([(iou_scores >= t).mean() * 100 for t in thresholds])
 
         # AP@25%
         y_true_25 = (iou_scores >= 25.0).astype(int)
-        ap25 = average_precision_score(y_true_25, conf_scores) * 100 if y_true_25.sum() > 0 else 0.0
-
+        ap25 = (
+            average_precision_score(y_true_25, conf_scores) * 100
+            if y_true_25.sum() > 0
+            else 0.0
+        )
 
         # AP@50%
         y_true_50 = (iou_scores >= 50.0).astype(int)
-        ap50 = average_precision_score(y_true_50, conf_scores) * 100 if y_true_50.sum() > 0 else 0.0
+        ap50 = (
+            average_precision_score(y_true_50, conf_scores) * 100
+            if y_true_50.sum() > 0
+            else 0.0
+        )
 
         # MEAN AP [50-100 %]
         ap_scores = []
@@ -167,69 +111,68 @@ def run_stat():
             else:
                 ap_t = 0.0
             ap_scores.append(ap_t)
-            
+
         map_score = np.mean(ap_scores) * 100
 
+        results.append(
+            {
+                "Method": f"Ours ({click} clicks)",
+                "mSR": f"{msr:.1f}",
+                "SR@50": f"{sr50:.1f}",
+                "SR@25": f"{sr25:.1f}",
+                "mAP": f"{map_score:.1f}",
+                "AP@50": f"{ap50:.1f}",
+                "AP@25": f"{ap25:.1f}",
+                "mIoU": f"{miou:.2f}",
+            }
+        )
 
-        results.append({
-            'Method': f'Ours ({click} clicks)',
-            'mSR': f"{msr:.1f}",
-            'SR@50': f"{sr50:.1f}",
-            'SR@25': f"{sr25:.1f}",
-            'mAP': f"{map_score:.1f}",
-            'AP@50': f"{ap50:.1f}",
-            'AP@25': f"{ap25:.1f}",
-            "mIoU" : f"{miou:.2f}"
-        })
-        
     results_df = pd.DataFrame(results)
 
     print(results_df.to_markdown(index=False))
 
     model_name = os.path.splitext(os.path.basename(config.pretraining_weights))[0]
-    save_path = f'results/{model_name}.md'
+    save_path = f"results/{model_name}.md"
 
-    with open(save_path, 'w') as f:
+    with open(save_path, "w") as f:
         f.write(results_df.to_markdown(index=False))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--generate_scenes_scannet', action=argparse.BooleanOptionalAction, default=False)
-    parser.add_argument('--generate_scenes_kitti', action=argparse.BooleanOptionalAction, default=False)
-    parser.add_argument('--run_interaction', action=argparse.BooleanOptionalAction, default=False)
-    parser.add_argument('--run_stats', action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument(
+        "--generate_scenes_kitti", action=argparse.BooleanOptionalAction, default=False
+    )
+    parser.add_argument(
+        "--run_interaction", action=argparse.BooleanOptionalAction, default=False
+    )
+    parser.add_argument(
+        "--run_stats", action=argparse.BooleanOptionalAction, default=False
+    )
 
     ###############
-    parser.add_argument('--pretraining_weights', type=str, default='weights/weights_exp14_3_pointN.pth')
-    parser.add_argument('--used_model', type=str, default='SmallHierarchicPointNetSeg') # MinkUNet34C, MinkUNet18B, MinkowskiPointNetSeg, HierarchicPointNetSeg, SmallHierarchicPointNetSeg
-    parser.add_argument('--dataset', type=str, default='kitti') # scannet, kitti
+    parser.add_argument(
+        "--pretraining_weights", type=str, default="weights/weights_exp14_3_pointN.pth"
+    )
+    parser.add_argument(
+        "--used_model", type=str, default="SmallHierarchicPointNetSeg"
+    )  # MinkUNet34C, MinkUNet18B, MinkowskiPointNetSeg, HierarchicPointNetSeg, SmallHierarchicPointNetSeg
+    parser.add_argument("--dataset", type=str, default="kitti")  # kitti
     ##############
 
     ## sequence num for kitti dataset, used with --run_interaction
-    parser.add_argument('--seq_num', type=str, default='00')
-
+    parser.add_argument("--seq_num", type=str, default="00")
 
     config = parser.parse_args()
 
-    if config.generate_scenes_scannet:
-        generate_scenes_scannet()
 
     if config.generate_scenes_kitti:
         generate_scenes_kitti()
 
     if config.run_interaction:
-        if config.dataset == 'scannet':
-            run_inter_scannet()
-        elif config.dataset == 'kitti':
+        if config.dataset == "kitti":
             run_inter_semkitti()
 
     if config.run_stats:
         run_stat()
-
-
-# python run_stat.py --run_stats
-# python run_stat.py --run_interaction
-
-# python run_stat.py --generate_scenes_scannet
-# python run_stat.py --generate_scenes_kitti
